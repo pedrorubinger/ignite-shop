c1928a5b90913f384f7f9fd65b40c310af51636e
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-02-18 21:59:43 -0300

    feat: add initial structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Image from "next/image";
+
+import LogoImg from "@/assets/Logo.svg";
+import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image alt="" src={LogoImg} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
